961e39322a5d94b4e018630474874dcf62f4ea7c
2eCxhd <2eCxhd@cyber-dojo.org>
2024-06-03 13:29:02 +0000

    28 ran tests, predicted green, got green

--- files/tuple.go
+++ files/tuple.go
@@ -5,7 +5,7 @@ type tuple [5]int
 func (t tuple) Max() (result int) {
     result = t[0]
     
-    for i = 1; i < len(t); i++ {
+    for i := 1; i < len(t); i++ {
         if t[i] > result {
             result = t[i]
         }
