0be84681fcee6564c4b4f3b3eda0a4cf7edf8ff6
2eCxhd <2eCxhd@cyber-dojo.org>
2024-06-03 09:21:11 +0000

    7 ran tests, predicted red, got red

---
+++ files/types.go
@@ -0,0 +1,9 @@
+package saddle_points
+
+type point struct {
+    x, y int
+}
+
+func Point(x, y int) point {
+    return point{}
+}
---
+++ files/types_test.go
@@ -0,0 +1,12 @@
+package saddle_points
+
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestPoint(t *testing.T) {
+    p := Point(1, 2)
+    
+    assert.Equal(t, 1, p.x, "unexpected X")
+}
