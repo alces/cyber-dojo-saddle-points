0121f3742fc422de4a5bc16f37276cb275dab5be
2eCxhd <2eCxhd@cyber-dojo.org>
2024-06-03 13:28:43 +0000

    27 ran tests, predicted green, got amber

--- files/tuple.go
+++ files/tuple.go
@@ -2,6 +2,14 @@ package saddle_points
 
 type tuple [5]int
 
-func (t tuple) Max() int {
-    return 0
+func (t tuple) Max() (result int) {
+    result = t[0]
+    
+    for i = 1; i < len(t); i++ {
+        if t[i] > result {
+            result = t[i]
+        }
+    }
+    
+    return
 }
