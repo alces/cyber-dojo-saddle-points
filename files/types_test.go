0b0d560567a3736a26d03d912aa3d3c2ced6146e
2eCxhd <2eCxhd@cyber-dojo.org>
2024-06-03 09:28:06 +0000

    12 ran tests, predicted red, got red

--- files/types_test.go
+++ files/types_test.go
@@ -5,7 +5,7 @@ import (
     "github.com/stretchr/testify/assert"
 )
 
-funct TestLandscape(t *testing.T) {
+func TestLandscape(t *testing.T) {
     l := Landscape({1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25})
     
     assert.Equal(t, 1, l[0][0])
