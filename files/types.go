d4ba2d69430e37091c3e6915ab3a0d021ca02121
2eCxhd <2eCxhd@cyber-dojo.org>
2024-06-03 09:27:53 +0000

    11 ran tests, predicted red, got red

--- files/types.go
+++ files/types.go
@@ -1,7 +1,13 @@
 package saddle_points
 
+type landscape [5][5]int
+
 type point struct {
     x, y int
+}
+
+func Landscape(data [25]int) landscape {
+    return landscape{}
 }
 
 func Point(x, y int) point {
