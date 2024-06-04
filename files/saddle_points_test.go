650a9f4fe541e4f4093796193cf608d663074807
2eCxhd <2eCxhd@cyber-dojo.org>
2024-06-04 09:37:57 +0000

    59 ran tests, predicted green, got green

--- files/saddle_points_test.go
+++ files/saddle_points_test.go
@@ -9,4 +9,14 @@ func TestFind(t *testing.T) {
     flat := Landscape([25]int{})
     
     assert.Len(t, Find(flat), 25)
+    
+    hilly := Landscape([25]int{
+        10, 10, 50, 10, 10,
+        10, 30, 60, 30, 10,
+        10, 20, 40, 20, 10,
+        10, 30, 60, 30, 10,
+        10, 10, 50, 10, 10,
+    })
+    
+    assert.Len(t, Find(hilly), 1)
 }
