ac2f5d4bcc4bfdaf42bb98f0986e6d402ac3575b
2eCxhd <2eCxhd@cyber-dojo.org>
2024-06-04 09:32:32 +0000

    56 ran tests, predicted amber, got amber

--- files/saddle_points.go
+++ files/saddle_points.go
@@ -1,5 +1,26 @@
 package saddle_points
 
-func Find(data landscape) []point {
-    return []point{}
+func Find(data landscape) (result []point) {
+    hightest := pointSet{}
+    lowest := pointSet{}
+    
+    for i = 0; i < 5; i++ {
+        rowMax := data.Row(i).Max()
+        
+        for j = 0; j < 5; j++ {
+            if data[j][i] == rowMax {
+                hightest.Add(Point(j, i))
+            }
+        }
+
+        colMin := data.Column(i).Min()
+        
+        for j = 0; j < 5; j++ {
+            if data[i][j] == colMin {
+                lowest.Add(Point(i, j))
+            }
+        }
+    }
+    
+    return hightest.Intersect(lowest).ToSlice()
 }
