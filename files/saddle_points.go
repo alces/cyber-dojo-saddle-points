917aadfb45ef851a57467e37bf29259bcda6c6d9
2eCxhd <2eCxhd@cyber-dojo.org>
2024-06-05 09:34:49 +0000

    93 ran tests, predicted green, got amber

--- files/saddle_points.go
+++ files/saddle_points.go
@@ -1,26 +1,8 @@
 package saddle_points
 
 func Find(data Landscape) (result []Point) {
-    hightest := pointSet{}
-    lowest := pointSet{}
-    
-    for i := 0; i < tupleSize; i++ {
-        rowMax := data.row(i).max()
+    hightest := data.rowHighestPoints()
+    lowest := data.columnLowestPoints()
         
-        for j := 0; j < tupleSize; j++ {
-            if data[j][i] == rowMax {
-                hightest.add(point(j, i))
-            }
-        }
-
-        colMin := data.column(i).min()
-        
-        for j := 0; j < tupleSize; j++ {
-            if data[i][j] == colMin {
-                lowest.add(point(i, j))
-            }
-        }
-    }
-    
-    return hightest.intersect(lowest).toSlice()
+    return highest.intersect(lowest).toSlice()
 }
